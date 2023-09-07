2fc3cf513cba165b95536604a40b82bdc18527ed
Abru660 <paulbrunet@live.fr>
2023-09-07 20:21:17 +0200

    lfs track

---
+++ Assets/Tests/editMode/InterfaceTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class InterfaceTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void InterfaceTestSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+}
---
+++ Assets/Tests/playMode/interfaceTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class interfaceTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void interfaceTestSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+}
